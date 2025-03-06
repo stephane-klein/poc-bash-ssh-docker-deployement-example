0ea5472833cb46b286f3c064f7491a0da57f2824
stephane-klein <contact@stephane-klein.info>
2025-03-06 17:32:39 +0100

    Add payload prefix

--- scripts/install_basic_server_configuration.sh
+++ scripts/install_basic_server_configuration.sh
@@ -3,4 +3,4 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-ssh root@$SERVER1_IP 'bash -s' < _install_basic_server_configuration.sh
+ssh root@$SERVER1_IP 'bash -s' < _payload_install_basic_server_configuration.sh
